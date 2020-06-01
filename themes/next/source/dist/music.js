451825e0c29417ca4850d34b8359ee6e2044a43e
abby.luo <abby.luo@starlight-sms.com>
2020-06-01 14:15:32 +0800

    music

---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,43 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "南山南",
+        artist: '排骨教主',
+        url: 'http://m8.music.126.net/20200601142854/a6c76d94679f15cc11985df777e7dcdf/ymusic/00d5/1c69/9a0f/f6af31e6629ba0a0b02368ea1a81b079.mp3',
+        cover: 'https://p3.music.126.net/J-5AzeDm8mOHmUXLWMy0OQ==/109951162822930517.jpg?param=300y300',
+      },
+      {
+        name: "半句再见",
+        artist: '孙燕姿',
+        url: 'http://m7.music.126.net/20200601143125/97496b6782c21ad452e5cfa922c0b631/ymusic/cd8b/0509/83a3/73cac6abd2be14cafdddb3f7b84be656.mp3',
+        cover: 'https://p3.music.126.net/PV630HGTPO4N49CLPTILLA==/109951163213836694.jpg?param=300y300',
+      },
+      {
+        name: "庐州月",
+        artist: '许嵩',
+        url: 'http://m7.music.126.net/20200601143151/18f72bab69e780ceb6b7aecb348a3c89/ymusic/86de/4d7b/c0dc/b45d9ddf9d7837f0d0bb71707ee1decf.mp3',
+        cover: 'https://p3.music.126.net/3hqcQrXZ39kDCCzV7QbZjA==/34084860473122.jpg?param=300y300',
+      },
+      {
+        name: "告白",
+        artist: '王欣宇',
+        url: 'http://m7.music.126.net/20200601143359/0a520e594027a5e390599f9a0c587823/ymusic/f52c/9235/590a/b6ff3dfe562b4c718bc7536b8ef018cc.mp3',
+        cover: 'https://p3.music.126.net/LJFLNLNzFjj9X_rfnsYZbg==/109951163689815304.jpg?param=300y300',
+      },
+      {
+        name: "沙发",
+        artist: '金玟岐',
+        url: 'http://m8.music.126.net/20200601143459/fd4577f416fd13dcbf5019cfb6bf076a/ymusic/08c0/0923/9931/60de8795fa7eae51a9151560ad1b6654.mp3',
+        cover: 'https://p3.music.126.net/MtofDr4IqworgZ7Ri3HY_g==/109951163064544587.jpg?param=300y300',
+      },
+      {
+        name: "来日方长",
+        artist: '李上安',
+        url: 'http://m7.music.126.net/20200601143851/21ea8401d598aac9c3f72980af3e2bcc/ymusic/2c20/031e/a0e2/67623e740b6b8a11b23c3ba10bda29d8.mp3',
+        cover: 'https://p3.music.126.net/8JrYu0edGVkDqITQfA0cig==/109951162920511105.jpg?param=300y300',
+      }
+    ]
+});
