d92ca9b48664b8bc2debe6acfcb70d7eeedce7b9
abby.luo <abby.luo@starlight-sms.com>
2020-06-03 11:36:53 +0800

    add dongban and links

---
+++ source/guestbook/index.md
@@ -0,0 +1,9 @@
+---
+title: 
+date: 2020-06-02 10:54:23
+comments: true
+---
+
+<blockquote class="blockquote-center">
+来都来了，说几句再走咯~<br/>((٩(´͈ᗨ`͈)۶))
+</blockquote>
--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -6,43 +6,43 @@ const ap = new APlayer({
       {
         name: "南山南",
         artist: '排骨教主',
-        url: 'http://m8.music.126.net/20200601142854/a6c76d94679f15cc11985df777e7dcdf/ymusic/00d5/1c69/9a0f/f6af31e6629ba0a0b02368ea1a81b079.mp3',
+        url: 'http://m8.music.126.net/20200602175425/f475a48d1330dcd9f2e1422a3abfda69/ymusic/00d5/1c69/9a0f/f6af31e6629ba0a0b02368ea1a81b079.mp3',
         cover: 'https://p3.music.126.net/J-5AzeDm8mOHmUXLWMy0OQ==/109951162822930517.jpg?param=300y300',
       },
       {
         name: "半句再见",
         artist: '孙燕姿',
-        url: 'http://m7.music.126.net/20200601143125/97496b6782c21ad452e5cfa922c0b631/ymusic/cd8b/0509/83a3/73cac6abd2be14cafdddb3f7b84be656.mp3',
+        url: 'http://m7.music.126.net/20200602175509/8d369c994b7e5edf2a2df2dca3e658e4/ymusic/cd8b/0509/83a3/73cac6abd2be14cafdddb3f7b84be656.mp3',
         cover: 'https://p3.music.126.net/PV630HGTPO4N49CLPTILLA==/109951163213836694.jpg?param=300y300',
       },
       {
         name: "庐州月",
         artist: '许嵩',
-        url: 'http://m7.music.126.net/20200601143151/18f72bab69e780ceb6b7aecb348a3c89/ymusic/86de/4d7b/c0dc/b45d9ddf9d7837f0d0bb71707ee1decf.mp3',
+        url: 'http://m7.music.126.net/20200602175525/65c318958e9f02b031c7c7c94c05418f/ymusic/86de/4d7b/c0dc/b45d9ddf9d7837f0d0bb71707ee1decf.mp3',
         cover: 'https://p3.music.126.net/3hqcQrXZ39kDCCzV7QbZjA==/34084860473122.jpg?param=300y300',
       },
       {
         name: "告白",
         artist: '王欣宇',
-        url: 'http://m7.music.126.net/20200601143359/0a520e594027a5e390599f9a0c587823/ymusic/f52c/9235/590a/b6ff3dfe562b4c718bc7536b8ef018cc.mp3',
+        url: 'http://m7.music.126.net/20200602175542/d0940e687c937c79f69928d2e8974533/ymusic/2c20/031e/a0e2/67623e740b6b8a11b23c3ba10bda29d8.mp3',
         cover: 'https://p3.music.126.net/LJFLNLNzFjj9X_rfnsYZbg==/109951163689815304.jpg?param=300y300',
       },
       {
         name: "沙发",
         artist: '金玟岐',
-        url: 'http://m8.music.126.net/20200601143459/fd4577f416fd13dcbf5019cfb6bf076a/ymusic/08c0/0923/9931/60de8795fa7eae51a9151560ad1b6654.mp3',
+        url: 'http://m8.music.126.net/20200602175618/43d1270e71b5d54b034d050586c92860/ymusic/08c0/0923/9931/60de8795fa7eae51a9151560ad1b6654.mp3',
         cover: 'https://p3.music.126.net/MtofDr4IqworgZ7Ri3HY_g==/109951163064544587.jpg?param=300y300',
       },
       {
         name: "来日方长",
         artist: '李上安',
-        url: 'http://m7.music.126.net/20200601143851/21ea8401d598aac9c3f72980af3e2bcc/ymusic/2c20/031e/a0e2/67623e740b6b8a11b23c3ba10bda29d8.mp3',
+        url: 'http://m7.music.126.net/20200602175542/d0940e687c937c79f69928d2e8974533/ymusic/2c20/031e/a0e2/67623e740b6b8a11b23c3ba10bda29d8.mp3',
         cover: 'https://p3.music.126.net/8JrYu0edGVkDqITQfA0cig==/109951162920511105.jpg?param=300y300',
       },
       {
         name: "像鱼",
         artist: '王贰浪',
-        url: 'http://m7.music.126.net/20200602104216/505cada2ca7191675ce3466fb5f816ee/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
+        url: 'http://m8.music.126.net/20200602175617/19c9ca719e310ced8951aa782bff9055/ymusic/555b/545f/005f/b193f4fbc9c5c3a94eb17531a71fbc26.mp3',
         cover: 'https://p3.music.126.net/ejEPGN6ulPSgCBXGq7dgqw==/109951163720047382.jpg?param=300y300',
       }
     ]
